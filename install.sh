0402fb9d8c1cb8c0672915787748465dfd936da9
Your Name <you@example.com>
2024-01-09 05:48:18 +0000

    .

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo cp -r tools/libgpiod_tools/ /usr/src/
 echo "Installing mdio"
 sudo cp -r tools/mdio_tools/ /usr/src/
 
-# echo "Installing nru220_startup_script_JP6.sh"
-# sudo chmod +x nru220_startup_script_JP6.sh
-# sudo cp nru220_startup_script_JP6.sh /etc/init.d
-# sudo update-rc.d nru220_startup_script_JP6.sh defaults
+echo "Installing nru220_startup_script_JP6.sh"
+sudo chmod +x nru220_startup_script_JP6.sh
+sudo cp nru220_startup_script_JP6.sh /etc/init.d
+sudo update-rc.d nru220_startup_script_JP6.sh defaults
