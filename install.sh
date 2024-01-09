03caa68f9c80a4b1e8bc2a74293322bef1019e6f
Your Name <you@example.com>
2024-01-09 05:23:52 +0000

    testing GPIO

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "Installing libgpioid"
-sudo cp -r libgpiod/ /usr/src/
+sudo cp -r tools/libgpiod_tools/ /usr/src/
 
 echo "Installing mdio"
-sudo cp mdio-tools/kernel/mdio-netlink.ko /usr/src/
-sudo cp mdio-tools/src/mdio/mdio /usr/src/ 
+sudo cp -r tools/mdio_tools/ /usr/src/
 
-echo "Installing nru220_startup_script_JP6.sh"
-sudo chmod +x nru220_startup_script_JP6.sh
-sudo cp nru220_startup_script_JP6.sh /etc/init.d
-sudo update-rc.d nru220_startup_script_JP6.sh defaults
+# echo "Installing nru220_startup_script_JP6.sh"
+# sudo chmod +x nru220_startup_script_JP6.sh
+# sudo cp nru220_startup_script_JP6.sh /etc/init.d
+# sudo update-rc.d nru220_startup_script_JP6.sh defaults
--- nru220_startup_script_JP6.sh
+++ nru220_startup_script_JP6.sh
@@ -12,49 +12,23 @@
 # ref: https://wiki.debian.org/LSBInitScripts
 
 
-function setGPO(){ 
-# $1=gpo_id $2=gpo_name #3=gpo_value
-    sudo echo $1 > /sys/class/gpio/export
-    sudo echo out > /sys/class/gpio/$2/direction
-    sudo echo $3 > /sys/class/gpio/$2/value
-    sleep 0.005
-}
-
-
-
-function getLineTegra234Gpio (){
-  # $1 = GPIO_NAME
-  GPIO_LINE=$(/usr/src/libgpiod/tools/gpioinfo -c 0 )
-  
-  
-  GPIO_PIN=$(sudo cat /sys/kernel/debug/gpio | grep $1 | awk -F"-| " '{print $3}')
-  echo $1 $GPIO_PIN
-  sudo echo $GPIO_PIN > /sys/class/gpio/export
-  sudo echo out > /sys/class/gpio/$1/direction
-  sudo echo 0 > /sys/class/gpio/$1/value
-  sleep 0.005
-}
 
 function setLineTegra234Gpio() {  
   # $1: GpioName
   # $2: GpioValue 
   GPIO_CHIP=0
-  GPIO_LINE=$(sudo /usr/src/libgpiod/tools/gpioinfo -c $GPIO_CHIP | grep $1 | awk -F":" '{print $1}' | awk -F" " '{print $2}')
-  sudo echo $1 $2
-  # sleep 0.5
-  # sudo /usr/src/libgpiod/tools/gpioinfo $1
-  sudo /usr/src/libgpiod/tools/gpioset -c $GPIO_CHIP --daemonize $GPIO_LINE=$2
+  GPIO_LINE=$(sudo /usr/src/libgpiod_tools/gpioinfo -c $GPIO_CHIP | grep $1 | awk -F":" '{print $1}' | awk -F" " '{print $2}')
+  sudo echo $1 $2  
+  sudo /usr/src/libgpiod_tools/gpioset -c $GPIO_CHIP --daemonize $GPIO_LINE=$2
 }
 
 function setLineTegra234GpioAon() {  
   # $1: GpioName
   # $2: GpioValue 
   GPIO_CHIP=1
-  GPIO_LINE=$(sudo /usr/src/libgpiod/tools/gpioinfo -c $GPIO_CHIP | grep $1 | awk -F":" '{print $1}' | awk -F" " '{print $2}')
-  sudo echo $1 $2
-  # sleep 0.5
-  # sudo /usr/src/libgpiod/tools/gpioinfo $1
-  sudo /usr/src/libgpiod/tools/gpioset -c $GPIO_CHIP --daemonize $GPIO_LINE=$2
+  GPIO_LINE=$(sudo /usr/src/libgpiod_tools/gpioinfo -c $GPIO_CHIP | grep $1 | awk -F":" '{print $1}' | awk -F" " '{print $2}')
+  sudo echo $1 $2  
+  sudo /usr/src/libgpiod_tools/gpioset -c $GPIO_CHIP --daemonize $GPIO_LINE=$2
 }
 
 
@@ -69,60 +43,56 @@ case $1 in
    
     # # MCU_BIOS_OK_ORIN _ to 0    
     # # setGPO 323 PAA.07 0    
-    # setLineTegra234GpioAon PAA.07 0 
+    setLineTegra234GpioAon PAA.07 0 
 
     # # OSLED _ to 1
-    # # GPIO3_PAA.04 _ gpio-320 
-    # # setGPO 320 PAA.04 1
-    # setLineTegra234GpioAon PAA.04 1
+    # # GPIO3_PAA.04 _ gpio-320     
+    setLineTegra234GpioAon PAA.04 1
     
     # # GPO_UART_EN _ to 1	
-    # # GPIO3_PAC.07 _ gpio-493 	
-    # # setGPO 493 PAC.07 1
-    # setLineTegra234Gpio PAC.07 1
+    # # GPIO3_PAC.07 _ gpio-493 	    
+    setLineTegra234Gpio PAC.07 1
 
     # # GPO_CAN_EN _ to 1
-    # # GPIO3_PBB.00 _ gpio-324 	
-    # # setGPO 324 PBB.00 1
-    # setLineTegra234GpioAon PBB.00 1
+    # # GPIO3_PBB.00 _ gpio-324 	    
+    setLineTegra234GpioAon PBB.00 1
     
     # # ---
     # # GPO_FAN_EN _ to 1	 
     # # PAC.05 _  gpio-491
-    # setLineTegra234Gpio PAC.05 1
+    setLineTegra234Gpio PAC.05 1
 
     # # GPO_RS232_EN _ to 1 _ Drive 1 after GPO_FAN_EN
     # # GPIO3_PBB.01 _  gpio-325 
-    # setLineTegra234GpioAon PBB.01 1
+    setLineTegra234GpioAon PBB.01 1
     
     # # ---
     # # GPO_PWR_POE_EN _ to 1
     # # GPIO3_PAC.01 _  gpio-487
-    # setLineTegra234Gpio PAC.01 1
+    setLineTegra234Gpio PAC.01 1
 
     # # GPO_PSE_RESET_N _ to 1 _ Drive 1 after GPO_PWR_POE_EN
     # # GPIO3_PBB.02 _  gpio-326
-    # setLineTegra234GpioAon PBB.02 1
-
+    setLineTegra234GpioAon PBB.02 1
 
     # ### Enable Marvell 88E6172
     # # sudo echo 388 > /sys/class/gpio/export
     # # sudo echo out > /sys/class/gpio/PG.05/direction
     # # sudo echo 1 > /sys/class/gpio/PG.05/value
-    # setLineTegra234Gpio PG.05 1
-    # sleep 0.1
-
-    sudo insmod /usr/src/mdio-netlink.ko
-
-    ifconfig eth0 up
+    setLineTegra234Gpio PG.05 1
     sleep 0.1
-    sudo /usr/src/mdio 2310000.ethernet phy 0x15 raw 1 0xc01e
+
+    sudo insmod /usr/src/mdio_tools/mdio-netlink.ko 
+
+    sudo ifconfig eth0 up
+    sleep 0.1
+    sudo /usr/src/mdio_tools/mdio 2310000.ethernet phy 0x15 raw 1 0xc01e
     sleep 0.2
-    sudo /usr/src/mdio 2310000.ethernet phy 0x15 raw 1 0xc03e
+    sudo /usr/src/mdio_tools/mdio  2310000.ethernet phy 0x15 raw 1 0xc03e
     sleep 0.2
 
-    ifconfig eth0 down
-    ifconfig eth0 up
+    sudo ifconfig eth0 down
+    sudo ifconfig eth0 up
 
     ### CAN Bus Configure
     modprobe can
